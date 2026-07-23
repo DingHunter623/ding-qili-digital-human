919ac65974229be461f565d11b888cd2a1a0132a
QilyLean <DingHunter623@gmail.com>
2026-07-23 13:19:21 +0800

    chore: refresh homepage copy cache

--- direct-navigation.js
+++ direct-navigation.js
@@ -11,7 +11,7 @@
   if (!document.getElementById('qilyLeanBrandIdentityScript')) {
     var brandScript = document.createElement('script');
     brandScript.id = 'qilyLeanBrandIdentityScript';
-    brandScript.src = '/brand-identity.js?v=20260723-qilylean-brand-v2';
+    brandScript.src = '/brand-identity.js?v=20260723-home-copy-v3';
     document.body.appendChild(brandScript);
   }
 })();
